71bd4a8744aa5a9e2cfeb2209632b335d82386f7
me <me@frontline.gifgroen.com>
2013-03-24 13:56:35 +0100

    production db pass changed

--- pieltjekwakkers/settings.py
+++ pieltjekwakkers/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'me',                      # Or path to database file if using sqlite3.
         'USER': 'me',                      # Not used with sqlite3.
-        'PASSWORD': '',                # Not used with sqlite3.
+        'PASSWORD': 'klopgeest',                # Not used with sqlite3.
         'HOST': 'localhost',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
